213420f157bfefcde772b5d4cc9e2c2cb5192ee8
Ron Sigal <rsigal@localhost.localdomain>
2023-10-03 17:45:47 -0400

    Multidimenstional arrays

--- testsuite/grpc-tests/src/test/java/org/jboss/resteasy/test/grpc/ArrayTest.java
+++ testsuite/grpc-tests/src/test/java/org/jboss/resteasy/test/grpc/ArrayTest.java
@@ -1,5 +1,15 @@
 package org.jboss.resteasy.test.grpc;
 
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import dev.resteasy.grpc.arrays.Array_proto.dev_resteasy_grpc_arrays___ArrayHolder;
+import dev.resteasy.grpc.bridge.runtime.protobuf.ArrayUtility;
+import dev.resteasy.grpc.bridge.runtime.protobuf.JavabufTranslator;
+import dev.resteasy.grpc.example.CC1JavabufTranslator;
+
 //import dev.resteasy.grpc.example.CC1JavabufTranslator;
 
 public class ArrayTest {
@@ -11,4 +21,38 @@ public class ArrayTest {
     //        System.out.println(ah);
     //        ArrayUtility.getArray(new CC1JavabufTranslator(), ah);
     //    }
+	
+//	@Test
+	public void testArrayUtility2() throws Exception {
+		try {
+//			Class.forName("LString");
+			Class<?> clazz = Class.forName("[Ljava.lang.Integer;");
+			System.out.println(clazz);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JavabufTranslator translator = new CC1JavabufTranslator();
+		int[][] intss = new int[][] { { 1, 2 }, { 3, 4 } };
+		System.out.println(intss.getClass());
+		Integer[][] intSS = new Integer[][] {{Integer.valueOf(1), Integer.valueOf(2)},{Integer.valueOf(3),Integer.valueOf(4)}};
+		System.out.println(intSS.getClass());
+		dev_resteasy_grpc_arrays___ArrayHolder holder = ArrayUtility.getHolder(translator, intss);
+		System.out.println(holder);
+		Integer[][] intSS2 = (Integer[][])ArrayUtility.getArray(translator, holder, "CC1_proto");
+		Assert.assertArrayEquals(intSS, intSS2);
+	}
+	
+	@Test
+	public void testArrayUtility3() throws Exception {
+		JavabufTranslator translator = new CC1JavabufTranslator();
+		Integer[][][] intSS = new Integer[][][]
+				{{{Integer.valueOf(1), Integer.valueOf(2)}, {Integer.valueOf(3),Integer.valueOf(4)}},
+				 {{Integer.valueOf(5), Integer.valueOf(6)}, {Integer.valueOf(7),Integer.valueOf(8)}}};
+
+		System.out.println(intSS.getClass());
+		dev_resteasy_grpc_arrays___ArrayHolder holder = ArrayUtility.getHolder(translator, intSS);
+		System.out.println(holder);
+		Integer[][][] intSS2 = (Integer[][][])ArrayUtility.getArray(translator, holder, "CC1_proto");
+		Assert.assertArrayEquals(intSS, intSS2);
+	}
 }
